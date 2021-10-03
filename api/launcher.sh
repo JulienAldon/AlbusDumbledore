bbb5481fd81751ff51004cb0494360ebdf3868ba
Julien <julien.aldon@epitech.eu>
2021-10-04 01:02:55 +0200

    fix launcher

--- api/launcher.sh
+++ api/launcher.sh
@@ -4,4 +4,4 @@ if [ -f .db-initialized ]; then
   touch .db-initialized
   pipenv run python populate_db.py
 fi
-pipenv run uvicorn main:app --host 0.0.0.0 --port 8081
+pipenv run uvicorn main:app --root-path /api --host 0.0.0.0 --port 8081
