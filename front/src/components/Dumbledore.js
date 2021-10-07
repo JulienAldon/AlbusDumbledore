064ca14904c2570258aeb38e520114ca5bf73d36
Arthur Lemaire <arthur.lemaire@epitech.eu>
2021-10-07 13:57:52 +0200

    Add bulk mode for points upload

--- front/src/components/Dumbledore.js
+++ front/src/components/Dumbledore.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState, useMemo, useRef } from 'react'
 import { useHistory } from 'react-router'
 import Select from 'react-select'
 import { useAuth } from '../hooks/use-auth'
@@ -18,10 +18,13 @@ const Dumbledore = () => {
 
     const history = useHistory()
 
+    const [bulkMode, setBulkMode] = useState(false)
     const [students, setStudents] = useState([])
     const [student, setStudent] = useState("")
     const [reason, setReason] = useState("")
     const [points, setPoints] = useState(0)
+
+    const fileInput = useRef()
 
     const options = useMemo(() => {
         return students.map(student => ({
@@ -55,15 +58,40 @@ const Dumbledore = () => {
         })
     }
 
+    const submitBulk = () => {
+        const data = new FormData()
+        console.log(fileInput.current.files[0])
+        data.append('file', fileInput.current.files[0])
+        fetch(`${config.url}/students/${points}?reason=${reason}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${auth.user.access_token}`,
+            },
+            body: data
+        }).then(res => {
+            if (res.status === 200)
+                history.push("/")
+        })
+    }
+
     return (
         <div className={styles.dumbledore}>
-            <label>Student</label>
-            <Select options={options} styles={customStyles} placeholder="Sélectionner l'étudiant à envoyer aux cachots" onChange={(e) => setStudent(e.value)}/>
+            <div>
+                <label htmlFor="bulk">bulk mode</label>
+                <input type="checkbox" checked={bulkMode} onChange={e => setBulkMode(!bulkMode)} id="bulk"/>
+            </div>
+            {bulkMode
+                ? <input placeholder="students list" type="file" accept=".csv" ref={fileInput}/>
+                : <>
+                    <label>Student</label>
+                    <Select options={options} styles={customStyles} placeholder="Sélectionner l'étudiant à envoyer aux cachots" onChange={(e) => setStudent(e.value)}/>
+                </>
+            }
             <label>Points</label>
             <input type="number" value={points} onChange={(e) => setPoints(e.target.value)}></input>
             <label>Reason</label>
             <input type="text" value={reason} onChange={(e) => setReason(e.target.value)}></input>
-            <button type="button" onClick={submit}>Let's go</button>
+            <button type="button" onClick={bulkMode ? submitBulk : submit}>Let's go</button>
         </div>
     )
 }
